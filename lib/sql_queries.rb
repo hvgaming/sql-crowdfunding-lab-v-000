354fe4f3cc1ce129af1911436ccd9009f2501531
hugo valois <hvgaming@yahoo.com>
2018-11-06 03:32:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,25 +18,6 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   GROUP BY users.name;"
 end
 
-def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal FROM projects
-  INNER JOIN pledges on projects.id = pledges.project_id
-  GROUP BY title HAVING SUM(pledges.amount) >= projects.funding_goal;"
-end
 
-def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-  "SELECT users.name, SUM(pledges.amount) FROM users
-  INNER JOIN pledges on users.id = pledges.user_id
-  GROUP BY name
-  ORDER BY SUM(amount), name;"
-end
 
-def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT projects.category, pledges.pledges FROM projects
-  INNER JOIN pledges on projects.id = pledges.project_id
-  GROUP BY category HAVING category = 'music';"
-end
 
-def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
-end
